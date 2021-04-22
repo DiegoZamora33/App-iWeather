ed3b027317bf0f625c02ddc016cbb9b9022f064a
DiegoZamora33 <diego.zamora33.dz@gmail.com>
2021-04-21 20:37:20 -0500

    version del 20 de Abril

--- App iWeather/ViewController.swift
+++ App iWeather/ViewController.swift
@@ -7,13 +7,127 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: - Outlet Conexiones y Variables Globales
+    @IBOutlet weak var inputCity: UITextField!
+    @IBOutlet weak var txtCiudad: UILabel!
+    @IBOutlet weak var iconWeather: UIImageView!
+    @IBOutlet weak var tempCity: UILabel!
+    
+    var isTouch = false
+    
+    
+    // MARK: - Action Conexiones
+    
+    /// Search Ciudad
+    @IBAction func searchCity(_ sender: UIButton) {
+        isTouch = true
+        inputCity.endEditing(true)
+        
+        if !inputCity.isEditing
+        {
+            if inputCity.text! != ""
+            {
+                /// Cambios Visuales
+                txtCiudad.text = inputCity.text
+                
+                
+                /// Relizar Consulta
+                realizarConsulta(city: inputCity.text!)
+                
+                
+                /// Clear Input
+                inputCity.text = ""
+            }
+            else
+            {
+                inputCity.placeholder = "Escribe una Ciudad"
+            }
+        }
+ 
+    }
+    
+    /// Boton GPS
+    @IBAction func locationBtn(_ sender: UIButton) {
+        
+    }
+    
+    // MARK: - Mis Funciones
+    /// * FUNCION PARA OCULTAR TECLADO *
+    @objc func tapGestureHandler() {
+        isTouch = false
+        inputCity.endEditing(true)
+      }
+    
+    /// * FUNCION PARA REALIZAR CONSULTA *
+    func realizarConsulta(city: String)
+    {
+        print("Find: \(city)")
+    }
+    
+    
+    
+    // MARK: - Overrides y Protocolos Func
+    
+    /// Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        /// Para poder Ocultar el teclado
+        let tapGesture = UITapGestureRecognizer(target: self, action:     #selector(tapGestureHandler))
+                view.addGestureRecognizer(tapGesture)
+        
+        /// Delegate Text Field
+        inputCity.delegate = self
     }
-
-
+    
+    /// Ir, teclado
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        isTouch = true
+        inputCity.endEditing(true)
+        return true
+    }
+    
+    
+    /// End Editing
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if inputCity.text! != ""
+        {
+            return true
+        }
+        else
+        {
+            inputCity.placeholder = "Escribe una Ciudad"
+            
+            if isTouch == false
+            {
+                return true
+            }
+            else
+            {
+                return false
+            }
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if isTouch == true
+        {
+            /// Cambios Visuales
+            txtCiudad.text = inputCity.text
+            
+            
+            /// Relizar Consulta
+            realizarConsulta(city: inputCity.text!)
+            
+            
+            /// Clear Input
+            inputCity.text = ""
+        }
+       
+    }
+    
 }
 
